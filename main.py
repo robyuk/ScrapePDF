7bbe43a4fb655f739a7d8e25745f595413589ce5
robertay <94378311+robyuk@users.noreply.github.com>
2022-06-27 14:19:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import fitz
+
+with fitz.open("students.pdf") as pdf:
+  
+  firstpage=pdf[0].get_text()  #get the first page text
+
+  allpages=''
+  for page in pdf:
+    print(20*'-')
+    print(page.get_text())
+    allpages=allpages+page.get_text() #get text from all pages
